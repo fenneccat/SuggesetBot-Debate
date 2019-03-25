43c289b03d7ca54f36bde2e9446d92ca9e4e7ca8
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2019-03-25 21:13:29 +0900

    trivial

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ HOST = 'localhost'
 PORT = 9200
 INDEX = 'kixx'
 FIELDS = ['title', 'text']
-document_retriever = DocumentRetriever(hosts=HOST, port=PORT, index=INDEX, fields=FIELDS)
+document_retriever = DocumentRetriever(
+    hosts=HOST, port=PORT, index=INDEX, fields=FIELDS)
 selector = SentenceSelector()
+
 
 def get_candidates(claim):
     docs = document_retriever.search(claim)['docs']
@@ -20,8 +22,9 @@ def get_candidates(claim):
         candidates += d['text']
     return candidates
 
+
 if __name__ == '__main__':
-    
+
     # test
     claim = """
         Resettlement is needed now, because an organized, legal route to hope in the U.S., as well as in Canada and Australia, will disempower the smugglers who are currently charging 1,200 euros for desperate people to get the six kilometers from Turkey to Greece.
